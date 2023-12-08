739d973e9390cb795e1c4a1599ff67ac26325590
ruu2023 <125800583+ruu2023@users.noreply.github.com>
2023-12-08 23:30:56 +0900

    summary

---
+++ Ruby/12.8-1.rb
@@ -0,0 +1,5 @@
+n = 1
+while n < 11
+  puts n
+  n = n + 1
+end
--- Ruby/problem8.rb
+++ Ruby/problem8.rb
@@ -1,11 +1,16 @@
 pnums = [2,3,5,7,11,13,17,21]
 n = 24
 while pnums.length < 102
+  is_prime = true
   pnums.each do |pnum|
     if n % pnum == 0
-      pnums << n
+      is_prime = false
+      break
     end
-    n = n + 1
   end
+
+  pnums << n if is_prime
+  n += 1
 end
-puts pnums[100]
+
+puts pnums[100] 
