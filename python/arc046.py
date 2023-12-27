1be86a9cfa99330ab65415a5e768e91dda37a756
ruu2023 <125800583+ruu2023@users.noreply.github.com>
2023-12-27 12:19:17 +0900

    Update arc046.py

--- python/arc046.py
+++ python/arc046.py
@@ -1,17 +1,30 @@
-import math
+# import math
+
+# N = int(input())
+
+# x = math.ceil(N / 9)
+
+# y = N % 9
+
+# if y == 0:
+#   y = 9
+
+# ans = ""
+
+# for _ in range(0, x):
+#   ans += str(y)
+
+# print(ans)
 
 N = int(input())
 
-x = math.ceil(N / 9)
+z = 0
 
-y = N % 9
+# 1から55555までの整数をすべて調べる
+for i in range(1, 55555 + 1):
 
-if y == 0:
-  y = 9
+  si = str(i)
 
-ans = ""
 
-for _ in range(0, x):
-  ans += str(y)
+  ok = True
 
-print(ans)
