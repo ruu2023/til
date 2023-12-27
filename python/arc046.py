9f42ec54ab3d648bfd92ecba96818dc80c05b173
ruu2023 <125800583+ruu2023@users.noreply.github.com>
2023-12-27 12:33:59 +0900

    Update arc046.py

--- python/arc046.py
+++ python/arc046.py
@@ -19,12 +19,23 @@
 N = int(input())
 
 z = 0
+ans = ""
 
 # 1から55555までの整数をすべて調べる
 for i in range(1, 55555 + 1):
 
   si = str(i)
 
-
   ok = True
 
+  for s in si:
+    if s != si[0]:
+      ok = False
+
+  if ok:
+    z += 1
+
+  if ok and z == N:
+    ans = i
+
+print(ans)
