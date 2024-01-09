a03ecb9799b5a2bace79a4fb48a359c087451c3f
ruu2023 <125800583+ruu2023@users.noreply.github.com>
2024-01-10 06:45:43 +0900

    Update abc083b.py

--- python/abc083b.py
+++ python/abc083b.py
@@ -1,8 +1,18 @@
 # 各桁を配列に格納する
-N = int(input())
-digits = []
-while N > 0:
-  digits.append( N % 10 )
-  N //= 10 #商を計算（少数切り捨て）
+# N = int(input())
+# digits = []
+# while N > 0:
+#   digits.append( N % 10 )
+#   N //= 10 #商を計算（少数切り捨て）
 
-print(digits)
+# print(digits)
+
+
+def calc_sum_digits(n):
+  sum_digit = 0
+  while n > 0:
+    sum_digit += n % 10
+    n //= 10
+  return sum_digit
+
+print(calc_sum_digits(4649))
